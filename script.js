0021282c23f5c6da603c96d6c7676d6a45f57d0d
Abdullah <ajjh564356165649@gmail.com>
2025-10-01 18:30:38 +0500

    done

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let b1 = document.querySelector(".b1")
 let b2 = document.querySelector(".b2")
 let b3 = document.querySelector(".b3")
+let b4 = document.querySelector(".b4")
 
 let second = document.querySelector(".sec")
 let mint = document.querySelector(".mint")
 let a = document.querySelector(".mili")
+let leapDiv = document.querySelector(".leap")
+let body = document.querySelector(".body")
 
 let secInterval = null;
 
-let timeLeft = 0; 
-let startTime; 
+let timeLeft = 0;
+let startTime;
 
 function startTimer() {
 
@@ -21,24 +24,24 @@ function startTimer() {
     secInterval = setInterval(() => {
 
         const duration = (new Date()).getTime() - startTime;
-        timeLeft = duration; 
+        timeLeft = duration;
 
         const totalSeconds = Math.floor(duration / 1000);
         const totalMinutes = Math.floor(totalSeconds / 60);
 
         const displayMili = duration % 1000;
         const displaySeconds = totalSeconds % 60;
-        const displayMinutes = totalMinutes % 60; 
+        const displayMinutes = totalMinutes % 60;
 
         second.textContent = displaySeconds < 10 ? `0${displaySeconds}` : `${displaySeconds}`;
 
         a.textContent = displayMili.toString().padStart(3, '0');
-        
+
 
         mint.textContent = displayMinutes < 10 ? `0${displayMinutes}:` : `${displayMinutes}:`;
 
-        }, 10)
-
+        return duration
+    }, 10)
 };
 
 function Stop_timer() {
@@ -48,19 +51,49 @@ function Stop_timer() {
 
 function Reset() {
     clearInterval(secInterval)
-    secInterval = null; 
-    timeLeft = 0;    
+    secInterval = null;
+    timeLeft = 0;
 
 
     second.textContent = `00`
     a.textContent = `000`
     mint.textContent = `00:`
-};
+
+
+    leapDiv.innerHTML=`<h1>LAPS</h1>`
+    leapDiv.style.display="none"
+    };
+
+function leap_value() {
+    let duration=timeLeft
+        const totalSeconds = Math.floor(duration / 1000);
+        const totalMinutes = Math.floor(totalSeconds / 60);
+
+        const displayMili = duration % 1000;
+        const displaySeconds = totalSeconds % 60;
+        const displayMinutes = totalMinutes % 60;
+        leapDiv.style.display="flex"
+    leapDiv.innerHTML += `          
+                                    <p style="color:black;
+                                    display:flex;
+                                    align-item:center;
+                                    justify-content:center;
+                                    margin: 0.5rem;
+                                    scroll-snap-type: mandatory;
+                                    background-color: #1B3C53;
+                                    padding:0.3rem;
+                                    border-radius:0.5rem;
+                                    width:25vw;"
+        >Mints:${displayMinutes} -- Seconds:${displaySeconds} -- MiliSec:${displayMili}</p>`
+        }
 
 
 b1.addEventListener("click", () => startTimer())
 b2.addEventListener("click", Stop_timer)
 b3.addEventListener("click", Reset)
+b4.addEventListener("click",leap_value)
+
+
 
 addEventListener("keydown", (e) => {
     if (e.code === "Space") {
@@ -78,5 +111,13 @@ addEventListener('keydown', (e) => {
     if (e.code === "Backspace") {
         Reset()
         e.preventDefault()
+    }
+})
+
+addEventListener("keydown",(e) =>{
+    if(e.code === "Enter"){
+        document.body.style.animation="main 1s ease-in-out forwards;"
+        document.body.style.justifyContent = "flex-start;"
+        leap_value()
     }
 })
